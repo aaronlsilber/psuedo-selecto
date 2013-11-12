3f4ca343f9e984938f62ae289b2bf2a070ac68de
Aaron Silber <aaronlsilber@gmail.com>
2013-11-11 23:45:52 -0500

    Editing classes for inline-block grid.

--- index.php
+++ index.php
@@ -7,11 +7,21 @@
 </head>
 <body>
 
-	<ul class="grid--1">
+	<h2 class="text--center">Floats</h2>
+
+	<div class="grid grid--1">
 		<?php for ($i=1; $i < 21; $i++) { ?>
-		<li class="grid__box"><?php echo $i; ?></li>
+		<div class="grid__box"><?php echo $i; ?></div>
 		<?php } ?>
-	</ul>
+	</div>
+
+	<h2 class="text--center">Inline-block</h2>
+
+	<div class="grid grid--2">
+		<?php for ($i=1; $i < 16; $i++) { ?>
+		<div class="grid__box"><?php echo $i; ?></div>
+		<?php } ?>
+	</div>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="js/app.js" type="text/javascript" charset="utf-8" async defer></script>
